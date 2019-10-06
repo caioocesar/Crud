4cb272eb412d7b6ff6702bcef9e3f180eee9fa6c
Caio César <caio.cesarsampaio@hotmail.com>
2019-10-06 12:37:56 -0300

    V5.1
    Remoção da opção de lembrar senha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -84,11 +84,7 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Esqueceu sua senha?') }}
-                                    </a>
-                                @endif
+                               
                             </div>
                         </div>
                     </form>
